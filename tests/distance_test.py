18de0c7b1c3c5b9adc5e88569adeffd46cd41ac8
Chris Westerman <chris.westerman8@gmail.com>
2020-11-08 15:13:49 +0000

    checking test with CD

--- tests/distance_test.py
+++ tests/distance_test.py
@@ -5,4 +5,4 @@ from mlproject2.distance import haversine
 import pytest
 
 def test_haversine():
-    assert haversine(48.865070, 2.380009, 48.235070, 2.393409) == 70.00789153218594
+    assert haversine(48.865070, 2.380009, 48.235070, 2.393409) == 71.00789153218594

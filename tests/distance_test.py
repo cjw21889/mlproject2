8095a9305a466dec08eb60c103b8c74bc25679fe
Chris Westerman <chris.westerman8@gmail.com>
2020-11-09 17:15:00 +0000

    added nltk test

--- tests/distance_test.py
+++ tests/distance_test.py
@@ -1,9 +1,12 @@
 # -*- coding: UTF-8 -*-
 
 # Import function to test
-from mlproject2.distance import haversine
+from mlproject2.distance import haversine, preprocessing
 import pytest
 
 def test_haversine():
     assert haversine(48.865070, 2.380009, 48.235070, 2.393409) == 70.00789153218594
     assert type(haversine(48.865070, 2.380009, 48.235070, 2.393409)) == float
+
+def test_preprocessing():
+    assert type(preprocessing('does this work')) == str

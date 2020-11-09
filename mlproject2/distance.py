8a90ddd37194e56ec10e1d50558f0fee2a40e511
Chris Westerman <chris.westerman8@gmail.com>
2020-11-09 17:31:07 +0000

    nltk final

--- mlproject2/distance.py
+++ mlproject2/distance.py
@@ -3,11 +3,10 @@ from math import radians, cos, sin, asin, sqrt
 import string
 import nltk
 
-nltk.download('stopwords')
+# nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('punkt')
 from nltk.corpus import stopwords
-# from mlproject2.nltkmodules import stopwords
 from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
--- mlproject2/nltkmodules.py
+++
@@ -1,6 +0,0 @@
-import nltk
-
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('punkt')
-from nltk.corpus import stopwords

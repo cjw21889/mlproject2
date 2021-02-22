91ce8336e5be22dc68ca40acb2334a919a12c878
Chris Westerman <chris.westerman8@gmail.com>
2021-02-22 10:16:25 +0000

    fixed test

--- tests/lib_test.py
+++ tests/lib_test.py
@@ -1,5 +1,16 @@
 # -*- coding: UTF-8 -*-
 
+# Import from standard library
+import os
+import mlproject2
+import pandas as pd
+# Import from our lib
+from mlproject2.lib import clean_data
+import pytest
+
+
+def test_clean_data():
+    1 == 1
 
 
 
